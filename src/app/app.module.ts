4d4ad8fcb9c9ea8e8d6e9a1d24275df28a91d9a7
Totiruzi <totiruzi@yahoo.com>
2022-04-04 10:55:50 +0300

    Generate component movie search and movie list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MovieSearchComponent } from './movie-search/movie-search.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieSearchComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule
